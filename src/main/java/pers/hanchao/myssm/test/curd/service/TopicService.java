a0530c428ebf7c56191b3987d2430a305f07a5f7
hanchao5272 <89298141@qq.com>
2018-05-01 00:15:47 +0800

    删除无用文件

--- src/main/java/pers/hanchao/myssm/test/curd/service/TopicService.java
+++
@@ -1,8 +0,0 @@
-package pers.hanchao.myssm.test.curd.service;
-
-import org.springframework.transaction.annotation.Transactional;
-
-public interface TopicService {
-    @Transactional
-    void curdTest();
-}
